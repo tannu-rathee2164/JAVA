3ceacb0fdaab2046a27d5413a19ee578a38bccef
tannu_rathee <165295417+tannu-rathee2164@users.noreply.github.com>
2024-05-23 16:54:25 +0530

    Create Pattern2.java
    * * * * *
    * * * * *
    * * * * *
    * * * * *
    * * * * *

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,15 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+        int n = 5;
+        int i = 1;
+        while (i <= n) {
+            int j = 1;
+            while (j <= n) {
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
