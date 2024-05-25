0b2086cdf7913257583fe553ce53c778d6fd6a23
tannu_rathee <165295417+tannu-rathee2164@users.noreply.github.com>
2024-05-25 16:14:05 +0530

    Create Pattern6.java
    * * * * *
      * * * *
        * * *
          * *
            *

---
+++ Patterns/Pattern6.java
@@ -0,0 +1,20 @@
+public class Pattern6 {
+    public static void main(String[] args) {
+        int n = 5;
+        int row = 1;
+        while( row <= n) {
+            int sp = 1;
+            while ( sp < row ) {
+                System.out.print("  ");
+                sp++;
+            }
+            int star = 5;
+            while ( star >= row) {
+                System.out.print("* ");
+                star--;
+            }
+            row++;
+            System.out.println();
+        }
+    }    
+}  
