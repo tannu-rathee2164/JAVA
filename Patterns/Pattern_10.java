d521166e64b102aae436678b87a8769e4cccdeab
tannu_rathee <165295417+tannu-rathee2164@users.noreply.github.com>
2024-05-26 14:19:31 +0530

    Create Pattern_10.java
    * * * * * * * * *
      * * * * * * *
        * * * * *
          * * *
            *

---
+++ Patterns/Pattern_10.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Pattern_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row = 1;
+        int star = n+n-1;
+        int sp = 0;  // Space
+        while (row <= n) {
+            int i = 1;
+            while(i <= sp) {
+                System.out.print("  ");
+                i++;
+            }
+            int j = 1;
+            while (j <= star) {
+                System.out.print("* ");
+                j++;
+            }
+            row++;
+            star -= 2;
+            sp++;
+            System.out.println();
+        }
+    }
+}
