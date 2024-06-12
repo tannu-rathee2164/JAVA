d62cc168fcc5cb1d3b1a2cad930340169994ff44
tannu_rathee <165295417+tannu-rathee2164@users.noreply.github.com>
2024-06-12 12:51:59 +0530

    Create Armstrong_num.java
    1634 is an Armstrong number as 1634 = 1^4 + 6^4 + 3^4 + 4^4
    
    371 is an Armstrong number as 371 = 3^3 + 7^3 + 1^3

---
+++ Function/Armstrong_num.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Armstrong_num {
+    public static boolean Is_Armstrong_Number(int n) {
+        int num = n;
+        int sum = 0;
+        while(n > 0) {
+            int rem = n % 10;
+            sum += (int) Math.pow(rem,pow);
+            n /= 10;
+        }
+        if(sum == num) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int CountOfDigit(int n) {
+        int count = 0;
+        while(n > 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(Is_Armstrong_Number(num));
+    }
+}
